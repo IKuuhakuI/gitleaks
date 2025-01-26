9cbfbdd49074b3e93188a4d605a739f181b14db2
IKuuhakuI <luizcarlosaguiarc@gmail.com>
2025-01-25 22:25:42 -0300

    exit with code 1 when error is found

--- index.js
+++ index.js
@@ -7,10 +7,14 @@ const {scanRepository} = require("./core/scanner");
 (async () => {
     try {
         const config = loadConfig();
-
         const results = await scanRepository(process.cwd(), config);
 
         reportResults(results, config);
+
+        if (results.length > 0) {
+            console.error("Sensitive data detected!");
+            process.exit(1);
+        }
     } catch (error) {
         console.error(`[ERROR] ${error.message}`);
         process.exit(1);
